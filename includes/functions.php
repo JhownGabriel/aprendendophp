6f2f436b4512a2e71d19197255bf7993c687feb2
JhownGabriel <gabriel4014@outlook.com>
2024-05-27 20:36:51 -0300

    php parte 16 pagina de resgistros

--- includes/functions.php
+++ includes/functions.php
@@ -120,3 +120,91 @@ function checkbrute($user_id, $mysqli) {
         }
     }
 }
+function login_check($mysqli) {
+    // Verifica se todas as variaveis das sessoes foram definidas
+
+    if (isset($_SESSION['user_id'], $_SESSION['username'], $_SESSION['login_string'])) {
+
+        $user_id = $_SESSION['user_id'];
+        $login_string = $_SESSION['login_string'];
+        $username = $_SESSION['username'];
+
+        // Pega a string do usuario.
+        $user_browser= $_SERVER['HTTP_USER_AGENT'];
+
+        if ($stmt = $mysqli->prepare("SELECT password FROM members WHERE id = ? LIMIT 1")) {
+
+            // Atribui "$user_id" ao parametro.
+            $stmt->bind_param('i', $user_id);
+            $stmt->execute(); //Executa uma query
+            $stmt->store_result();
+
+            if ($stmt->num_rows == 1) {
+
+                // Caso o usuario exista, pega variaveis a partir do resultado.
+                $stmt->bind_result($password);
+                
+                $stmt->fetch();
+                $login_check = hash('sha512',$password.$user_browser);
+
+                if ($login_check == $login_string) {
+
+                    // Logado!!!
+                    return true;
+                } else {
+                    
+                    // Nao foi logado
+                    return false;
+                }
+            } else {
+
+                // Nao foi logado
+                return false;
+            } 
+        } else {
+
+            // Nao foi logado
+            return false;
+        } 
+    } else {
+
+        //Nao foi logado
+        return false;
+    }
+}
+
+function esc_url($url) {
+
+    if (" == $url) {
+        return $url;
+    }
+
+    $url = preg_replace('|[^a-z0-9~+_.?#=!&;,/:%@$\|*\'()\\x80-xff]|i'',",$url);
+
+    $strip = array('%0d','%0a','%0D','%0A');
+    $url = (string) $url;
+
+
+    $count = 1;
+    while ($count) {
+        $url = str_replace($strip, '',$url,$count);
+    }
+
+    $url = str_replace(';//','://', $url);
+
+    $url = htmlentities($url);
+
+
+    $url = str_replace('&amp;', '&#038;', $url);
+    $url = str_replace("", '&#039;', $url);
+
+    if ($url[0] !== '/') {
+
+        // Estamos interessados em links relacionados provenientes de 
+        // $_SERVER['PHP_SELF']
+
+        return ";
+    } else {
+        return $url;
+    }
+}
